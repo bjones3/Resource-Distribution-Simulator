228117fd9d090daa074fa63f89220602a43d6577
finndobbs <finndobbs@ku.edu>
2021-02-14 18:36:51 -0800

    Split Building .hpp files to .hpp and .cpp files

--- inc/Building/FulfillmentCenter.hpp
+++ inc/Building/FulfillmentCenter.hpp
@@ -4,42 +4,18 @@
 #ifndef FULFILLMENTCENTER_HPP
 #define FULFILLMENTCENTER_HPP
 
-class FulfillmentCenter: public Building
+class FulfillmentCenter: 
+
+public Building
 {
 
-FulfillmentCenter(int x, int y)
-{
-  xPos = x;
-  yPos = y;
-  volumeCapacity = 500000;
-  occupantCapacity = 0;
-  contentVolume = 0;
-  std::list<Drone> drones;
-  droneCapacity = 100;
-}
+FulfillmentCenter(int x, int y);
 
-CargoDrone createResourceDrone()
-{
+CargoDrone createResourceDrone();
 
- return new CargoDrone(xPos,yPos);
+PassengerDrone createPassengerDrone();
 
-}
-
-PassengerDrone createPassengerDrone()
-{
-
-  return new PassengerDrone(xPos,yPos);
-
-}
-
-Resource createResource(Resource & resource)
-{
-
-  return resource;
-
-}
-
-};
+Resource createResource(Resource & resource);
 
 private:
 	int droneCapacity;
