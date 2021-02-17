8b3db8617bd05d9b757c8bc2caf3792402b40533
flobbus <flobbus@gmail.com>
2021-02-16 22:27:06 -0600

    Revert "implement hash member variables in buildings instead of lists"
    This reverts commit 9c6146d4d57e3d5fde40a38af6a4ca6673fdf04f.

--- src/Building/Building.cpp
+++ src/Building/Building.cpp
@@ -1,144 +1,124 @@
 #include "../../inc/rds.hpp"
 
 /*
-   protected:
-   int xPos;
-   int yPos;
-   long long int id;
-   std::list<Individual> occupants;
-   std::list<Resource> contents;
-   int occupantCapacity;
-   double contentVolumeCapacity;
-   double contentVolume;
- */
+protected:
+	int xPos;
+	int yPos;
+	long long int id;
+	std::list<Individual> occupants;
+	std::list<Resource> contents;
+	int occupantCapacity;
+	double contentVolumeCapacity;
+	double contentVolume;
+*/
 
 Building::Building()
 {
-    xPos = -1;
-    yPos = -1;
-    id = -1;
-    occupantCapacity = 0;
-    contentVolumeCapacity = 0;
-    contentVolume = 0;
+	xPos = -1;
+	yPos = -1;
+	id = -1;
+	occupantCapacity = 0;
+	contentVolumeCapacity = 0;
+	contentVolume = 0;
 }
 
 Building::Building(int x, int y)
 {
-    xPos = x;
-    yPos = y;
-    id = -1;//id = ID::generateID();
-    occupantCapacity = 0;
-    contentVolumeCapacity = 0;
-    contentVolume = 0;
+	xPos = x;
+	yPos = y;
+	id = -1;//id = ID::generateID();
+	occupantCapacity = 0;
+	contentVolumeCapacity = 0;
+	contentVolume = 0;
 }
 
 int Building::getOccupantAmount()
 {
-    return occupants.size();
+	return occupants.size();
 }
 
 int Building::getOccupantCapacity()
 {
-    return occupantCapacity;
+	return occupantCapacity;
 }
 
 bool Building::canAddOccupant(Individual & occupant)
 {
-    if(occupants.contains(occupant.id)) //requires c++20
-        return false;
-    if(occupants.size() == occupantCapacity)
-        return false;
-    if(occupant.getPossessionVolume() + contentVolume > contentVolumeCapacity)
-        return false;
-    return true;
+	if(occupants.size() == occupantCapacity)
+		return false;
+	if(occupant.getPossessionVolume() + contentVolume > contentVolumeCapacity)
+		return false;
+	return true;
 }
 
 void Building::addOccupant(Individual & occupant)
 {
-    if(canAddOccupant(occupant))
-        occupants.push_front(occupant);
+	if(canAddOccupant(occupant))
+		occupants.push_front(occupant);
 }
 
 //TODO: Change occupants list to a hash
 Individual Building::removeOccupant(Individual & occupant)
 {
-    /*std::list<Individual>::iterator temp = std::find(occupants.begin(),occupants.end(),occupant);
-      Individual removedOccupant = *temp;
-      for()
-      if(*temp.getID() == occupant.getID())
-      occupants.remove(*temp);
-      return removedOccupant;*/
-    if(occupants.contains(occupant.id)) //requires c++20
-    {
-        occupants.erase(occupant.id);
-        return occupant;
-    }
-    else
-        return NULL;
+	/*std::list<Individual>::iterator temp = std::find(occupants.begin(),occupants.end(),occupant);
+	Individual removedOccupant = *temp;
+	for()
+		if(*temp.getID() == occupant.getID())
+			occupants.remove(*temp);
+	return removedOccupant;*/
 }
 
 bool Building::canAddResource(Resource & resource)
 {
-    if(resources.contains(resource.id))
-        return false;
-    if (resource.getVolume() + contentVolume > contentVolumeCapacity)
-        return false;
-    return true;
+	if (resource.getVolume() + contentVolume > contentVolumeCapacity)
+		return false;
+	return true;
 }
 
 void Building::addResource(Resource & resource)
 {
-    if (canAddResource(resource))
-    {
-        contentVolume += resource.getVolume();
-        contents.push_front(resource);
-    }
+	if (canAddResource(resource))
+	{
+		contentVolume += resource.getVolume();
+		contents.push_front(resource);
+	}
 }
 
 Resource Building::removeResource(Resource & resource)
 {
-    /*std::list<Resource>::iterator temp = std::find(contents.begin(),contents.end(),resource);
-      Resource removedResource = *temp;
-      contents.remove(*temp);
-      contentVolume -= resource.getVolume();
-      return removedResource;*/
-      if(resources.contains(resource.id)) //requires c++20
-	  {
-        resources.erase(resource.id);
-		return resource;
-	  }
-      else
-        return NULL;
-
-
+	/*std::list<Resource>::iterator temp = std::find(contents.begin(),contents.end(),resource);
+	Resource removedResource = *temp;
+	contents.remove(*temp);
+	contentVolume -= resource.getVolume();
+	return removedResource;*/
 }
 
 bool Building::canBringOccupant(Individual & occupant)
 {
-    if(occupants.size() == occupantCapacity)
-        return false;
-    if(occupant.getPossessionVolume() + contentVolume > contentVolumeCapacity)
-        return false;
-    return true;
+	if(occupants.size() == occupantCapacity)
+		return false;
+	if(occupant.getPossessionVolume() + contentVolume > contentVolumeCapacity)
+		return false;
+	return true;
 }
 
 bool Building::canBringContents(Resource & resource)
 {
-    if(resource.getVolume() + contentVolume > contentVolumeCapacity)
-        return false;
+	if(resource.getVolume() + contentVolume > contentVolumeCapacity)
+		return false;
 }
 
 int Building::getXPos()
 {
-    return xPos;
+	return xPos;
 }
 
 int Building::getYPos()
 {
-    return yPos;
+	return yPos;
 }
 
 long long int Building::getID()
 {
-    return id;
+	return id;
 }
