9c6146d4d57e3d5fde40a38af6a4ca6673fdf04f
flobbus <flobbus@gmail.com>
2021-02-16 21:43:01 -0600

    implement hash member variables in buildings instead of lists

--- inc/Building/Building.hpp
+++ inc/Building/Building.hpp
@@ -1,37 +1,38 @@
 #ifndef BUILDING_HPP
 # define BUILDING_HPP
 
+#include <unordered_map>
 # include "../rds.hpp"
 
 class Building
 {
-	protected:
-		int						xPos;
-		int						yPos;
-		long long int			id;
-		std::list<Individual>	occupants;
-		std::list<Resource>		contents;
-		int						occupantCapacity;
-		double					contentVolumeCapacity;
-		double					contentVolume;
+    protected:
+        int						xPos;
+        int						yPos;
+        long long int			id;
+        std::unordered_map<long long int, Individual> occupants;
+        std::unordered_map<long long int, Resource> contents;
+        int						occupantCapacity;
+        double					contentVolumeCapacity;
+        double					contentVolume;
 
-	public:
-		Building();
-		Building(int x, int y);
-		int				getOccupantAmount();
-		int				getOccupantCapacity();
-		bool			canAddOccupant(Individual & occupant);
-		void			addOccupant(Individual & occupant);
-		//TODO: Change occupants list to a hash
-		Individual		removeOccupant(Individual & occupant);
-		bool			canAddResource(Resource & resource);
-		void			addResource(Resource & resource);
-		Resource		removeResource(Resource & resource);
-		bool			canBringOccupant(Individual & occupant);
-		bool			canBringContents(Resource & resource);
-		int				getXPos();
-		int				getYPos();
-		long long int	getID();
+    public:
+        Building();
+        Building(int x, int y);
+        int				getOccupantAmount();
+        int				getOccupantCapacity();
+        bool			canAddOccupant(Individual & occupant);
+        void			addOccupant(Individual & occupant);
+        //TODO: Change occupants list to a hash
+        Individual		removeOccupant(Individual & occupant);
+        bool			canAddResource(Resource & resource);
+        void			addResource(Resource & resource);
+        Resource		removeResource(Resource & resource);
+        bool			canBringOccupant(Individual & occupant);
+        bool			canBringContents(Resource & resource);
+        int				getXPos();
+        int				getYPos();
+        long long int	getID();
 };
 
 #endif
