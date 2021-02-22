4080d30e187f990752a5754198352d5825f48421
God-of-TecSing <60628231+God-of-TecSing@users.noreply.github.com>
2021-02-21 21:48:14 -0600

    added agenda classes

---
+++ inc/Agenda/Agenda.hpp
@@ -0,0 +1,22 @@
+#include <list>
+
+#ifndef AGENDA_HPP
+#define AGENDA_HPP
+
+class Agenda
+{
+public:
+  Agenda(Individual individual);
+  Agenda(std::list<Event> events, Individual individual);
+  void executeEvent();
+  bool canExecuteEvent();
+  void addEvent(Event event);
+  void addEvents(std::list<Event> events);
+  void removeEvent(Event event);
+
+private:
+  std::list<Event> m_events;
+  Individual m_individual;
+};
+
+#endif
