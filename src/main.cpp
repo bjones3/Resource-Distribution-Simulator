a102ceaa08183edc3e91e2491c4d1cbf484d1f78
Ryan Boyce <r211b673@ku.edu>
2021-04-11 22:36:32 -0500

    Add forking to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include "../inc/rds.hpp"
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main( int argc, char **argv, char **envp )
 {
-	Executive ex;
-	ex.run();
+	//int** droneInfo = new int*[100];
+	
+	int pid = fork();
+	if(pid < 0)
+	{
+		std::cout << "Unable to fork\n";
+		return 0;
+	}
+	else if(pid == 0)
+	{
+		//std::cout << "Forked child\n";
+		std::string graphics = "ls";	//Change this to the path to the graphics exe
+		char* args[2];
+		args[0] = const_cast<char*>(graphics.c_str());
+		args[1] = (char*)NULL;
+		execvp(graphics.c_str(),args);
+		std::cout << "Exec failed\n";
+	}
+	else
+	{
+		//waitpid(pid,NULL,0);
+		//std::cout << "Forked parent\n";
+		Executive ex;
+		ex.run();
+	}
 }
 
